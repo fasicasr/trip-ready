9d1620bf48d26823a71e3133132914353814f789
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-07-17 12:36:39 -0400

    initial set up for client and server sides

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const {  } = require('../models');
+
+const resolvers = {
+  Query: {
+    //code goes here...
+  },
+  Mutation: {
+    //CRUD code goes here
+  },
+};
+
+module.exports = resolvers;
