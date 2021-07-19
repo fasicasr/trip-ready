7f8648259a32117055e922e580dd4ee9abca7632
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-07-18 23:22:22 -0400

    Server side updates: 1. created models 2. created schemas with resolvers/typeDefs 3. added utils folder with dateFormat file 4. created seeders for User objects

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Group = require('./Group');
+const Picture = require('./Picture');
+const Suggestion = require('./Suggestion');
+
+module.exports = { User, Group, Picture, Suggestion};
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,40 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  //code goes here
+  type User {
+    _id: ID
+    email: String
+    firstName: String
+    lastName: String
+  }
+
+  type Suggestion {
+    _id: ID
+    title: String
+    description: String
+    suggestedUser: User
+    createdAt: String
+  }
+
+  type Picture {
+    _id: ID
+    title: String
+    description: String
+    createdAt: String
+    owner: User
+  }
+
+  type Group {
+    groupName: String
+    destination: String
+    users: [User]
+    suggestions: [Suggestion]
+  }
+
+  type Query {
+    users: [User]
+    
+  }
 `;
 
 module.exports = typeDefs;
