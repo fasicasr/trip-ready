a1532d99c66a7186c1db84c7de95b582aaf6b280
Dmitry <dmitry.balduev@gmail.com>
2021-07-28 18:16:03 -0400

    .

--- client/src/Profile/ProfileCaption.js
+++ client/src/Profile/ProfileCaption.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
+import { usePlacesContext } from '../components/Navbar';
+
+
 function ProfileCaption() {
   const [editMode, setEditMode] = React.useState(false);
+  // const { places } = usePlacesContext();
 
   return (
     <div className="profilecaption">
+
+      {/* {places.map((place) => (
+        <div>key={place.name}
+        {place.name} : {place.formatted_address}
+        </div>
+      ))} */}
+
       <h1>Profile Caption</h1>
       <form>
         <div>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import {Button, Navbar, Form, Nav, FormControl} from "react-bootstrap"
 
+const PlacesContext = createContext();
+
+export const usePlacesContext = () => useContext(PlacesContext);
+
+export const PlacesProvider = ({ children }) => {
+  const initialState = {
+    places: [
+      {
+        
+        name: '',
+        business_status: "",
+        formatted_address: "",
+        place_id: "",
+      },
+    ],
+  };
+
+  // Provider components expect a value prop to be passed
+  return (
+    <PlacesContext.Provider value={initialState}>
+      {/* Render children passed from props */}
+      {children}
+    </PlacesContext.Provider>
+  );
+};
+
+
+
 function NavBar() {
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const destination = document.getElementById("destination-input").value;
+    const key = 'AIzaSyCH-vDcWNCDsoG4WXCJ8S9cpFf3jpzzF54';
+    const requestUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+${destination}&key=${key}`;
+
+    fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+    });
+  };
+
   return (
   <Navbar bg="dark" variant="dark" expand="lg">
    
@@ -15,14 +58,15 @@ function NavBar() {
         <Nav.Link href="#action2">Logout</Nav.Link>
     
       </Nav>
-      <Form className="d-flex">
+      <Form className="d-flex" onSubmit={handleSearch}>
         <FormControl
           type="search"
           placeholder="Enter destination"
           className="mr-2"
           aria-label="Search"
+          id="destination-input"
         />
-        <Button variant="outline-secondary">Search</Button>
+        <Button variant="outline-secondary" id="search-button" >Search</Button>
       </Form>
     </Navbar.Collapse>
   </Navbar>
