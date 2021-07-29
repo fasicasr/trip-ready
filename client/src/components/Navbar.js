4b2ad2e4f933dc57ddac4aa89c47eadcac533e53
Dmitry <dmitry.balduev@gmail.com>
2021-07-28 23:18:20 -0400

    .

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,8 @@
-<<<<<<< HEAD
+
 import React, { useState, createContext, useContext } from 'react';
 import {Button, Navbar, Form, Nav, FormControl} from "react-bootstrap"
+import { propTypes } from "react-bootstrap/esm/Image";
+
 
 const PlacesContext = createContext();
 
@@ -10,7 +12,6 @@ export const PlacesProvider = ({ children }) => {
   const initialState = {
     places: [
       {
-        
         name: '',
         business_status: "",
         formatted_address: "",
@@ -31,7 +32,9 @@ export const PlacesProvider = ({ children }) => {
 
 
 function NavBar() {
-  const handleSearch = (event) => {
+  const searchRef = React.useRef();
+
+  const handleSearchClick = (event) => {
     event.preventDefault();
     const destination = document.getElementById("destination-input").value;
     const key = 'AIzaSyCH-vDcWNCDsoG4WXCJ8S9cpFf3jpzzF54';
@@ -43,72 +46,38 @@ function NavBar() {
     })
     .then(function (data) {
       console.log(data)
+      props.setSearchData(searchRef.current.value);
     });
   };
 
   return (
-  <Navbar bg="dark" variant="dark" expand="lg">
-   
+    <Navbar bg="dark" variant="dark" expand="lg">
     <Navbar.Collapse id="navbarScroll">
       <Nav
         className="mr-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
+        style={{ maxHeight: "100px" }}
         navbarScroll
       >
         <Nav.Link href="#action1">Home</Nav.Link>
         <Nav.Link href="#action2">Logout</Nav.Link>
-    
       </Nav>
-      <Form className="d-flex" onSubmit={handleSearch}>
+      <Form className="d-flex">
         <FormControl
           type="search"
           placeholder="Enter destination"
           className="mr-2"
           aria-label="Search"
+          ref={searchRef}
           id="destination-input"
         />
-        <Button variant="outline-secondary" id="search-button" >Search</Button>
+        <Button variant="outline-secondary" onClick={handleSearchClick}>
+          Search
+        </Button>
       </Form>
     </Navbar.Collapse>
   </Navbar>
-=======
-import React from "react";
-import { Button, Navbar, Form, Nav, FormControl } from "react-bootstrap";
-import { propTypes } from "react-bootstrap/esm/Image";
+  )
+  } 
 
-function NavBar(props) {
-  const searchRef = React.useRef();
-  const handleSearchClick = (event) => {
-    //fetch data from api using this input value here - pass in api results in setSearchData
-    props.setSearchData(searchRef.current.value);
-  };
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Collapse id="navbarScroll">
-        <Nav
-          className="mr-auto my-2 my-lg-0"
-          style={{ maxHeight: "100px" }}
-          navbarScroll
-        >
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Logout</Nav.Link>
-        </Nav>
-        <Form className="d-flex">
-          <FormControl
-            type="search"
-            placeholder="Enter destination"
-            className="mr-2"
-            aria-label="Search"
-            ref={searchRef}
-          />
-          <Button variant="outline-secondary" onClick={handleSearchClick}>
-            Search
-          </Button>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
->>>>>>> c3df397d3e3d1144b08f83d6ac23869b921e710e
-  );
-}
 
 export default NavBar;
